81b913505a0ee14b4b2c0e013c0a86e8cb4376ba
Obidjon Toshev <toshevobidjon@gmail.com>
2024-04-17 13:39:50 +0500

    Update dc_completion.bash

--- dc_completion.bash
+++ dc_completion.bash
@@ -1,12 +1,47 @@
 #!/usr/bin/env bash
 
+if [ -f /usr/share/bash-completion/completions/docker ]; then
+    . /usr/share/bash-completion/completions/docker
+fi
+
 _dc() {
     COMPREPLY=();
 
-    local word="${COMP_WORDS[COMP_CWORD]}"
+    if declare -F _init_completion >/dev/null 2>&1; then
+        _init_completion -n =: || return
+    else
+        COMPREPLY=()
+        _get_comp_words_by_ref "$@" cur prev words cword
+    fi
 
-    if [ "$COMP_CWORD" -eq 1 ]; then
-        COMPREPLY=($(compgen -W "$(dc -l)" -- "$word"))
+    __docker_debug
+    __docker_debug "========= starting completion logic =========="
+    __docker_debug "cur is ${cur}, words[*] is ${words[*]}, #words[@] is ${#words[@]}, cword is $cword"
+
+    rwords=("${words[@]:0:$cword+1}")
+    __docker_debug "Real words[*]: ${rwords[*]},"
+
+    words=("docker" "compose")
+
+    if [ $cword == 1 ]; then
+        if [ "${cur:0:1}" != "-" ]; then
+            COMPREPLY=($(compgen -W "$(dc -l)" -- "${cur}"))
+        else 
+            local list=("--help" "--new" "-n" "--remove" "-r" "--list" "-l")
+            COMPREPLY=($(compgen -W "${list[*]}" -- "${cur}"))
+        fi
+    else 
+        words=("${words[@]}" "-f" "$HOME/.config/dc/yml/${rwords[1]}" "${rwords[@]:2:${#rwords[@]}}")
+        __docker_debug "words:" ${words[@]}
+        cword=$(($cword + 2))
+        __docker_debug "cword:" ${cword[@]}
+        prev=${words[@]:0}
+        __docker_debug "prev:" ${prev[@]}
+
+        local out directive
+        __docker_get_completion_results
+        __docker_process_completion_results
     fi
 }
 
+complete -F _dc dc
